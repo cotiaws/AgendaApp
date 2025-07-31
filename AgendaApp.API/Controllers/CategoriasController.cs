3bc08bdbeaa598d523a0303bcc10ef484040ec1c
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2025-07-31 19:53:46 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaApp.API/Controllers/CategoriasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
